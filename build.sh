39bc086755548ff1e77e286acbf1f46c3c4d32da
Hiroyuki Wada <wadahiro@gmail.com>
2017-11-14 15:56:04 +0900

    Add build script for release

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VERSION=0.3
+
+DIR=$(cd $(dirname $0); pwd)
+cd $DIR
+
+rm -rf bin/*
+go build -v -o bin/transproxy -a -tags netgo -installsuffix netgo cmd/transproxy/main.go
+
+tar cvzf go-transproxy-$VERSION.tar.gz bin README.md LICENSE
+
